ca0a0f72ff18a01770e39092385bc34d3e86fa3d
Phillip <phillipables1@gmail.com>
2018-03-25 16:48:06 -0400

    declare new TextView array called mDots, new method addDotsIndicator which sets mDots as new TextView[3], for each position setText to Html.fromHtml and set Size and Color

--- app/src/main/java/com/example/techtron/myapplication/MainActivity.java
+++ app/src/main/java/com/example/techtron/myapplication/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.techtron.myapplication;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPagerSlide;
     LinearLayout linearLayoutDot;
+
+    private TextView[] mDots;
+
+    private SliderAdapter sliderAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         viewPagerSlide = (ViewPager) findViewById(R.id.viewPagerSlide);
         linearLayoutDot = (LinearLayout) findViewById(R.id.linearLayoutDot);
+
+        sliderAdapter = new SliderAdapter(this);
+
+        viewPagerSlide.setAdapter(sliderAdapter);
+    }
+
+    public void addDotsIndicator(){
+
+        mDots = new TextView[3];
+
+        for(int i =0; i < mDots.length; i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+
+            
+
+        }
     }
 }
