53753522e774738293beaa96189057091b17da9a
Phillip <phillipables1@gmail.com>
2018-03-25 13:56:12 -0400

    declare view pager and linear layout, onCreate set value to R.id

--- app/src/main/java/com/example/techtron/myapplication/MainActivity.java
+++ app/src/main/java/com/example/techtron/myapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.techtron.myapplication;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    ViewPager viewPagerSlide;
+    LinearLayout linearLayoutDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPagerSlide = (ViewPager) findViewById(R.id.viewPagerSlide);
+        linearLayoutDot = (LinearLayout) findViewById(R.id.linearLayoutDot);
     }
 }
