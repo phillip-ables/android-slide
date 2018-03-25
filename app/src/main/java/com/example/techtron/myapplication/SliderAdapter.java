78852487e7aa953f6552e2ada9c09d08c7be5105
Phillip <phillipables1@gmail.com>
2018-03-25 15:54:26 -0400

    declare context and layoutInflater variable then in class constructor for SlideAdapter set this context

--- app/src/main/java/com/example/techtron/myapplication/SliderAdapter.java
+++ app/src/main/java/com/example/techtron/myapplication/SliderAdapter.java
@@ -1,9 +1,18 @@
 package com.example.techtron.myapplication;
 
+import android.content.Context;
 import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class SliderAdapter extends PagerAdapter {
+    Context context;
+    LayoutInflater layoutInflater;
+
+   public SliderAdapter(Context context) {
+       this.context = context;
+   }
+
     @Override
     public int getCount() {
         return 0;
@@ -13,4 +22,5 @@ public class SliderAdapter extends PagerAdapter {
     public boolean isViewFromObject(View view, Object object) {
         return false;
     }
+
 }
